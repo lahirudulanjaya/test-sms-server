8422b81c514d981fada4914ad1d29c6df3501ed7
lahirudulanjaya <laahirudulanjaya@gmail.com>
2021-06-06 15:35:36 +0530

    react-create-app on Heroku

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,12 @@
+
+<
+src/index.js,f/9/f900edc44cb929c2267a16100a32851f6d5b477d
+<
+package.json,7/0/7030d0b2f71b999ff89a343de08c414af32fc93a
+A
+package-lock.json,f/a/fa288d1472d29beccb489a676f68739ad365fc47
+:
+
+src/App.js,1/e/1e43fa63aecd2127be034b3fb9d4caa172275714
+A
+public/index.html,f/4/f43ecdf8d666ee908aacce037b4cd72159e4741b
---
+++ src/App.js
@@ -0,0 +1,130 @@
+import React, {Component} from 'react';
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Icon from "@material-ui/core/Icon";
+
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            senders: '',
+            message: ''
+        }
+    }
+    componentDidMount() {
+
+    }
+
+    onChange = (e) => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+    sendSMS(e) {
+        e.preventDefault();
+        const senders = this.state.senders.split(',')
+        fetch(
+            'https://sms-server0.herokuapp.com/sms/sendSMS',
+            {
+                method: 'PUT',
+                headers: {
+                    'Authorization': 'Bearer ' + localStorage.getItem('token'),
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    senders: senders,
+                    message: this.state.message,
+                    apiKey:this.state.apiKey
+                }),
+            },
+        ).then(response => {
+            if(response.ok){
+                alert('Successfully send the messages')
+            }
+            else{
+                if(response.status===403)
+                {
+                    alert('You do not have have permission')
+                }
+            }
+        })
+    }
+
+    render() {
+        return (
+            <Container component="main"  style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh',
+                width:'100%'
+
+            }}>
+                <CssBaseline/>
+                <div  >
+                    <Typography component="h1" variant="h5" style={{textAlign:'center'}}>
+                        Testing Send SMS. Make sure your sms server is running.
+                    </Typography>
+                    <div>
+                        <form noValidate>
+                            <TextField
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="senders"
+                                label="senders"
+                                name="senders"
+                                autoComplete="senders"
+                                autoFocus
+                                value={this.state.senders}
+                                onChange={(e) => this.onChange(e)}
+                            />
+                            <TextField
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="apiKey"
+                                label="API Key"
+                                name="apiKey"
+                                autoComplete="senders"
+                                autoFocus
+                                value={this.state.apiKey}
+                                onChange={(e) => this.onChange(e)}
+                            />
+                            <TextField
+                                variant="outlined"
+                                margin="normal"
+                                multiline
+                                rows={4}
+                                rowsMax={10}
+                                required
+                                fullWidth
+                                name="message"
+                                label="message"
+                                id="message"
+                                autoComplete="message"
+                                value={this.state.message}
+                                onChange={(e) => this.onChange(e)}
+                            />
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                endIcon={<Icon>send</Icon>}
+                                onClick={(e) => this.sendSMS(e)}
+                            >send</Button>
+                        </form>
+                    </div>
+                </div>
+            </Container>
+        );
+    }
+}
+
+
+export default App;
