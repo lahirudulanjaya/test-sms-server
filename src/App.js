d695834d105295f2c683987240703978de3358a9
AsithaIndrajith <asithaindrajithk9@gmail.com>
2021-06-06 17:07:29 +0530

    ui changes

--- src/App.js
+++ src/App.js
@@ -1,131 +1,191 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Typography from "@material-ui/core/Typography";
 import Icon from "@material-ui/core/Icon";
-
+import { withSnackbar } from "notistack";
+import { Link } from "@material-ui/core";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      senders: "",
+      message: "",
+      apiKey: "",
+      loading: false,
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            senders: '',
-            message: '',
-            apiKey: ''
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  sendSMS = (e) => {
+    this.setState({
+      loading: true,
+    });
+    e.preventDefault();
+    if (
+      this.state.senders === "" ||
+      this.state.message === "" ||
+      this.state.apiKey === ""
+    ) {
+      this.setState({
+        loading: false,
+      });
+      this.props.enqueueSnackbar("Invalid data", { variant: "error" });
+      return;
+    }
+    const senders = this.state.senders.split(",");
+    fetch("https://meghadutha.dhahas.com/sms/sendSMS", {
+      method: "PUT",
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("token"),
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        senders: senders,
+        message: this.state.message,
+        apiKey: this.state.apiKey,
+      }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          this.props.enqueueSnackbar("SMS sent successfully", {
+            variant: "success",
+          });
+          this.setState({
+            loading: false,
+          });
+        } else {
+          this.props.enqueueSnackbar("Invalid API key", { variant: "error" });
+          this.setState({
+            loading: false,
+          });
         }
-    }
-    componentDidMount() {
+      })
+      .catch((err) => {
+        this.props.enqueueSnackbar("Something went wrong", {
+          variant: "error",
+        });
+        this.setState({
+          loading: false,
+        });
+      });
+  };
 
-    }
-
-    onChange = (e) => {
-        this.setState({[e.target.name]: e.target.value})
-    }
-
-    sendSMS(e) {
-        e.preventDefault();
-        const senders = this.state.senders.split(',')
-        fetch(
-            'https://sms-server0.herokuapp.com/sms/sendSMS',
-            {
-                method: 'PUT',
-                headers: {
-                    'Authorization': 'Bearer ' + localStorage.getItem('token'),
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    senders: senders,
-                    message: this.state.message,
-                    apiKey:this.state.apiKey
-                }),
-            },
-        ).then(response => {
-            if(response.ok){
-                alert('Successfully send the messages')
-            }
-            else{
-                if(response.status===403)
-                {
-                    alert('You do not have have permission')
-                }
-            }
-        })
-    }
-
-    render() {
-        return (
-            <Container component="main"  style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '100vh',
-                width:'100%'
-
-            }}>
-                <CssBaseline/>
-                <div  >
-                    <Typography component="h1" variant="h5" style={{textAlign:'center'}}>
-                        Testing Send SMS. Make sure your sms server is running.
-                    </Typography>
-                    <div>
-                        <form noValidate>
-                            <TextField
-                                variant="outlined"
-                                margin="normal"
-                                required
-                                fullWidth
-                                id="senders"
-                                label="senders"
-                                name="senders"
-                                autoComplete="senders"
-                                autoFocus
-                                value={this.state.senders}
-                                onChange={(e) => this.onChange(e)}
-                            />
-                            <TextField
-                                variant="outlined"
-                                margin="normal"
-                                required
-                                fullWidth
-                                id="apiKey"
-                                label="API Key"
-                                name="apiKey"
-                                autoComplete="senders"
-                                autoFocus
-                                value={this.state.apiKey}
-                                onChange={(e) => this.onChange(e)}
-                            />
-                            <TextField
-                                variant="outlined"
-                                margin="normal"
-                                multiline
-                                rows={4}
-                                rowsMax={10}
-                                required
-                                fullWidth
-                                name="message"
-                                label="message"
-                                id="message"
-                                autoComplete="message"
-                                value={this.state.message}
-                                onChange={(e) => this.onChange(e)}
-                            />
-                            <Button
-                                variant="contained"
-                                color="primary"
-                                endIcon={<Icon>send</Icon>}
-                                onClick={(e) => this.sendSMS(e)}
-                            >send</Button>
-                        </form>
-                    </div>
-                </div>
-            </Container>
-        );
-    }
+  render() {
+    return (
+      <Container
+        component="main"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          width: "100%",
+        }}
+      >
+        <CssBaseline />
+        <div style={{ width: "50%" }}>
+          <Typography
+            component="h1"
+            variant="h5"
+            style={{ textAlign: "center" }}
+          >
+            Meghadutha SMS Server Testing
+          </Typography>
+          <Typography
+            component="h6"
+            variant="body1"
+            style={{
+              textAlign: "left",
+              fontStyle: "italic",
+              fontSize: 15,
+              marginTop: 15,
+            }}
+          >
+            NOTE: Make sure your{" "}
+            <Link
+              target="_blank"
+              href={
+                "https://play.google.com/store/apps/details?id=com.dhahas.smsserver"
+              }
+            >
+              Meghadutha Mobile SMS Server
+            </Link>{" "}
+            is running and{" "}
+            <Link
+              target="_blank"
+              href={
+                "https://github.com/lahirudulanjaya/SendSMS/releases/tag/0.0.1"
+              }
+            >
+              Meghadutha SMS Tool
+            </Link>{" "}
+            is installed.
+          </Typography>
+          <div>
+            <form noValidate>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="senders"
+                label="senders"
+                name="senders"
+                autoComplete="senders"
+                autoFocus
+                value={this.state.senders}
+                onChange={(e) => this.onChange(e)}
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="apiKey"
+                label="API Key"
+                name="apiKey"
+                autoComplete="senders"
+                autoFocus
+                value={this.state.apiKey}
+                onChange={(e) => this.onChange(e)}
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                multiline
+                rows={4}
+                rowsMax={10}
+                required
+                fullWidth
+                name="message"
+                label="message"
+                id="message"
+                autoComplete="message"
+                value={this.state.message}
+                onChange={(e) => this.onChange(e)}
+              />
+              <Button
+                disabled={this.state.loading}
+                variant="contained"
+                color="primary"
+                endIcon={<Icon>send</Icon>}
+                onClick={(e) => this.sendSMS(e)}
+              >
+                send
+              </Button>
+            </form>
+          </div>
+        </div>
+      </Container>
+    );
+  }
 }
 
-
-export default App;
+export default withSnackbar(App);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import '@fortawesome/fontawesome-free/css/all.min.css'; import
-    'bootstrap-css-only/css/bootstrap.min.css'; import
-    'mdbreact/dist/css/mdb.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { SnackbarProvider } from "notistack";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SnackbarProvider
+      maxSnack={3}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+    >
+      <App />
+    </SnackbarProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
