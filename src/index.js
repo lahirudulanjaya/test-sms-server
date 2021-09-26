f827a05e7af85fcd1ce9ad4161856400ce4acc63
lahirudulanjaya <laahirudulanajaya@gmail.com>
2021-09-26 17:38:29 +0530

    remove reportWebVitals

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { SnackbarProvider } from "notistack";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
@@ -23,7 +22,3 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
